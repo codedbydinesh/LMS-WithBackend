44507355a4b286abfc76e3b7b8ace714cdbf7e6c
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-12-05 22:52:37 +0530

    feat: set up DB connection and update server initialization

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { clerkWebHooks } from './controllers/webhooks.controllers.js';
+import connectDB from './db/mongodb.js';
 
 dotenv.config({
     path: './.env'
@@ -9,23 +11,33 @@ dotenv.config({
 // Initialize Express 
 const app = express();
 
+// Database Connection
 
 
 // Middlewares
 app.use(cors());
+app.use(express.json());
 
 // Routes
 
 app.get('/', ( req, res) =>{
         res.send("API Working")
     }
-)
+);
+
+app.post('/clerk', clerkWebHooks);
 
 // port
 
-const PORT = process.env.PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
-    
+// Start the server and connect to DB
+connectDB()
+.then(() => {
+    app.listen(PORT, ()=> {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Failed to connect to DB", error);
 })
