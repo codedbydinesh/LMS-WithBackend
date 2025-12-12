94bfbe79cac86e865b94d0bda40400d99a4be774
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-12-12 20:15:52 +0530

    feat: add educator dashboard data and earnings calculation

--- server/src/controllers/educator.controllers.js
+++ server/src/controllers/educator.controllers.js
@@ -1,6 +1,7 @@
 import {clerkClient} from '@clerk/express'
 import {v2 as cloudinary} from 'cloudinary';
 import { Course } from '../models/course.model.js';
+import { Purchase } from '../models/purchase.model.js';
 
 // Update role to educator
 export const updateRoleToEducator = async (req, res) => {
@@ -47,3 +48,59 @@ export const addCourse =  async (req, res) => {
         res.status(500).json({success:false, message: error.message});
     }
 }
+
+// Get Educator Courses
+export const getEducatorCourses = async (req, res) => {
+    try {
+        const educator = req.auth.userId;
+
+        const courses = await Course.find({educator});
+
+        res.status(200).json({success:true, courses});
+
+    } catch (error) {
+        res.status(500).json({success:false, message: error.message});
+    }
+}
+
+// Get Educator Dashboard Data (Total Earnings, Enrolled Students, No. of Courses)
+
+export const getEducatorDashboardData = async (req, res) => {
+    try {
+        const educator = req.auth.userId;
+
+        const courses = await Course.find({educator});
+        const totalCourses = courses.length;
+
+        const courseIds = courses.map(course => course._id);
+        const purchases = await Purchase.find({courseId: {$in: courseIds}, status: 'completed'});
+        const totalEarnings = purchases.reduce((total, purchase) => total + purchase.amount, 0);
+
+        // Get unique enrolled student IDs with their titles 
+        const enrolledStudentsData = [];
+
+        // Iterate through each course to get enrolled students
+        for(const course of courses){
+            const students = await User.find({_id: {$in: course.enrolledStudents}}, 'name imageUrl');
+
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                })
+            })
+        }
+
+        // Send Response
+        res.status(200).json({success:true, DashboardData: {
+            totalCourses,
+            totalEarnings,
+            enrolledStudentsData
+        }});
+    } catch (error) {
+        res.status(500).json({success:false, message: error.message});
+    }
+};
+
+
+
