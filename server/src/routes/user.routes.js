8eecd211a1ab38635d9f9d071ae723596ab54c96
DineshKumarKori <dineshkumarkori978@gmail.com>
2026-05-21 07:35:32 +0530

    feat: add course progress tracking and user rating functionality

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getUserData, purchaseCourse, userEnrolledCourses } from "../controllers/user.Controller.js";
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from "../controllers/user.Controller.js";
 
 const userRouter = Router();
 
 userRouter.get('/data', getUserData);
 userRouter.get('/enrolled-courses', userEnrolledCourses);
 userRouter.post('/purchase', purchaseCourse);
+
+userRouter.post('/update-course-progress', updateUserCourseProgress);
+userRouter.post('/get-course-progress',getUserCourseProgress);
+userRouter.post('add-rating', addUserRating);
 
 export default userRouter;
